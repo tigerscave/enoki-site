ce799162212b4ffbbd49e267b921c68765267532
Masanori Enoki <enoki@adawarp.com>
2020-04-24 15:48:39 +0900

    modified js

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const menuBtn = document.getElementById("menu-btn")
 const mobileMenu = document.getElementById("mobile-menu")
 
 menuBtn.addEventListener("click", () => {
-  if (mobileMenu.classList.contains("active") === false) {
-    mobileMenu.classList.add("active")
-  } else {
+  if (mobileMenu.classList.contains("active")) {
     mobileMenu.classList.remove("active")
+  } else {
+    mobileMenu.classList.add("active")
   }
 });
 
